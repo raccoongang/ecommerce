ae10261ba712f53ceccbcfdac48f6e269bdeade2
otecMuxah <suxx_2000@hotmail.com>
2017-12-18 13:37:37 +0000

    markup fixes done
    revert devstack.py and fix last line in footer
    
    revert devstack py
    
    revert devstack py
    
    remove stripe

--- ecommerce/settings/devstack.py
+++ ecommerce/settings/devstack.py
@@ -14,8 +14,8 @@ LOGGING['handlers']['local'] = {
 
 SOCIAL_AUTH_REDIRECT_IS_HTTPS = False
 
-# TODO Remove this once we convert the E-Commerce service to use the latest JWT_ISSUERS configuration.
-JWT_AUTH['JWT_DECODE_HANDLER'] = 'edx_rest_framework_extensions.utils.jwt_decode_handler'
+# TODO Remove this once we convert the E-Commerce service to use the latest JWT_ISSUERS configuration.		
+JWT_AUTH['JWT_DECODE_HANDLER'] = 'edx_rest_framework_extensions.utils.jwt_decode_handler'		
 
 # Allow live changes to JS and CSS
 COMPRESS_OFFLINE = False
@@ -24,17 +24,17 @@ COMPRESS_ENABLED = False
 # PAYMENT PROCESSING
 PAYMENT_PROCESSOR_CONFIG = {
     'edx': {
-        # NOTE: The same profile information is used here to appease the payment processor class.
+        # NOTE: The same profile information is used here to appease the payment processor class.		
         # Only Silent Order POST is actually used.
         'cybersource': {
             'merchant_id': 'edx_org',
             'transaction_key': '/yIJJejEGoNNcecTyxC9ZD0wR2ZjkkKuOaZnq2BGMGIGQIOKA1rBR009OuvKbPW4J1KLb15BMlaoiUXoj/8/Fp6dy33/aHAU0+yGKcEMxyYXQOBPKjuoChIlMRVkrtWZqP9shGxw1jwHNovmGrvd2ULRIn21Rsq6YnHie7lLLRhXyY2MjnFXfv75eH2rFwfi4hBPbVPvx/r8PwgFIh5otAzsgyIlBjaKJkzbNXd5qCOdNFSBcPcJps3YgVH0ASleI/SZp+Ckuyotd+EhzK0tOehPJAm3L03lkPNeFX9lcemuRkeV53V3nvobn3GaX0td4FAEe8CZBn+IpFC2PoK0tw==',
             'soap_api_url': 'https://ics2wstest.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.115.wsdl',
-            'profile_id': '00D31C4B-4E8F-4E9F-A6B9-1DB8C7C86223',
-            'access_key': '90a39534dc513e8a81222b158378dda1',
-            'secret_key': 'ff09d545ddbe4f1e908cc47e3cceb30e4e9ff57a1fe0493392b69a0b75f8ac3df7840f89131d46faa4487071d53576d25047ebb39e9b4af18e9fb5ee1d4f1f66fdb711284c844c4c82bd24f168781e786ecf8b2d3dba4ab5b543c188ca5728e00b8ace43cca14cefbb605ecdc0706eda4cd50785d5754fd691426ddff03fcc7b',
-            'payment_page_url': 'https://testsecureacceptance.cybersource.com/pay',
-            'receipt_path': PAYMENT_PROCESSOR_RECEIPT_PATH,
+            'profile_id': '00D31C4B-4E8F-4E9F-A6B9-1DB8C7C86223',		
+-           'access_key': '90a39534dc513e8a81222b158378dda1',		
+-           'secret_key': 'ff09d545ddbe4f1e908cc47e3cceb30e4e9ff57a1fe0493392b69a0b75f8ac3df7840f89131d46faa4487071d53576d25047ebb39e9b4af18e9fb5ee1d4f1f66fdb711284c844c4c82bd24f168781e786ecf8b2d3dba4ab5b543c188ca5728e00b8ace43cca14cefbb605ecdc0706eda4cd50785d5754fd691426ddff03fcc7b',		
+-           'payment_page_url': 'https://testsecureacceptance.cybersource.com/pay',		
+-           'receipt_path': PAYMENT_PROCESSOR_RECEIPT_PATH,
             'cancel_checkout_path': PAYMENT_PROCESSOR_CANCEL_PATH,
             'send_level_2_3_details': True,
             'sop_profile_id': '00D31C4B-4E8F-4E9F-A6B9-1DB8C7C86223',
