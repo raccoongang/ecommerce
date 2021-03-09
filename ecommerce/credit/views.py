7f738b7298bad826025bf65356ba46ce56b1b500
mykola-s <mykola.sukhariev@raccoongang.com>
2021-03-09 04:41:17 +0200

    [OP-270] removed pdb

--- ecommerce/credit/views.py
+++ ecommerce/credit/views.py
@@ -150,7 +150,6 @@ class Checkout(TemplateView):
             new_price = None
             discount = None
             if code:
-                import pdb; pdb.set_trace()
                 discount = format_benefit_value(voucher.benefit, self.request.site.siteconfiguration.currency)
                 if discount_type == 'Percentage':
                     new_price = stockrecord.price_excl_tax - (stockrecord.price_excl_tax * (discount_value / 100))
