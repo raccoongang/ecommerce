e3b25613628d926effb8c79d8e8f5ebdee1c8d9b
mykola-s <mykola.sukhariev@raccoongang.com>
2021-03-13 13:35:14 +0200

    [OP-270] added currency endpoint

---
+++ ecommerce/extensions/api/v2/views/currency.py
@@ -0,0 +1,12 @@
+"""HTTP endpoint to get the currency of the current site."""
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ecommerce.extensions.api.serializers import CurrencySerializer
+
+
+class CurrencyAPIView(APIView):
+    serializer_class = CurrencySerializer
+    
+    def get(self, request, *args, **kwargs):
+        return Response({'currency': self.request.site.siteconfiguration.currency})
