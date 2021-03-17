2c3fe4e7b58b268d68e3ed8e6771fe843136f1e4
mykola-s <mykola.sukhariev@raccoongang.com>
2021-03-17 16:25:52 +0200

    [OP-270] revert LANGUAGE_COOKIE_NAME

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -476,7 +476,7 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 # Detailed information at: https://docs.djangoproject.com/en/dev/ref/settings/
 SESSION_COOKIE_NAME = 'ecommerce_sessionid'
 CSRF_COOKIE_NAME = 'ecommerce_csrftoken'
-LANGUAGE_COOKIE_NAME = 'openedx-language-preference'
+LANGUAGE_COOKIE_NAME = 'ecommerce_language'
 SESSION_COOKIE_SECURE = False
 # END COOKIE CONFIGURATION
 
