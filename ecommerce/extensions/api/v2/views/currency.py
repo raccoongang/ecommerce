72f5832a8a14d15615416121bedc2cc0d4fbb846
mykola-s <mykola.sukhariev@raccoongang.com>
2021-03-23 11:42:11 +0200

    [OP-270] fix currency endpoint

--- ecommerce/extensions/api/v2/views/currency.py
+++ ecommerce/extensions/api/v2/views/currency.py
@@ -1,4 +1,5 @@
 """HTTP endpoint to get the currency of the current site."""
+from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,4 +10,4 @@ class CurrencyAPIView(APIView):
     serializer_class = CurrencySerializer
     
     def get(self, request, *args, **kwargs):
-        return Response({'currency': self.request.site.siteconfiguration.currency})
+        return Response({'currency': self.request.site.siteconfiguration.currency or settings.OSCAR_DEFAULT_CURRENCY})
